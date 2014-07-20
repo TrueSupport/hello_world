f50065effff79fd86e777d5d54db8145adee1947
Ruslan Soromlya <garenauser@meta.ua>
2014-07-20 13:21:09 +0300

    starts empty ruby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWorld::Application.config.secret_key_base = 'fa7667a4501686e59b7be4010294b0a93f0022627744f8428ee85d95c2e897e9b840da23c5e7fbfa608c6cdc9d33dbd3d73e4d1accb692df96f4dcb324c4d02b'
